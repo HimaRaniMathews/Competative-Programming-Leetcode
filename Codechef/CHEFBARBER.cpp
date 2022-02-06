916a38b6be36fe5df0f427be02cb849205ee9a64
Hima Rani Mathews <68076769+HimaRaniMathews@users.noreply.github.com>
2022-02-06 23:16:18 +0530

    Create CHEFBARBER.cpp

---
+++ Codechef/CHEFBARBER.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,m;
+	    cin>>n>>m;
+	    int res=n*m;
+	    cout<<res<<"\n";
+	}
+	return 0;
+}
