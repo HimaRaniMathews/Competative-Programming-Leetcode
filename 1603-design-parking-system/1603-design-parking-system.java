350821d73a21a0d3bcf5c31c6784952f69e73a11
Hima Rani Mathews <68076769+HimaRaniMathews@users.noreply.github.com>
2022-02-24 23:28:25 +0530

    Time: 9 ms (63.69%), Space: 54.6 MB (12.92%) - LeetHub

---
+++ 1603-design-parking-system/1603-design-parking-system.java
@@ -0,0 +1,26 @@
+class ParkingSystem {
+    int big,medium,small;
+    public ParkingSystem(int big, int medium, int small) 
+    {
+        this.big=big;
+        this.medium=medium;
+        this.small=small;
+    }
+    
+    public boolean addCar(int carType) 
+    {
+        if(carType==1){
+            if(big==0)return false;
+            big--;
+        }else if(carType==2){
+            if(medium==0)return false;
+            medium--;
+            
+        }else{
+            if(small==0)return false;
+            small--;
+            
+        }
+        return true;
+    }
+}
