7d7a2bfd63f34e727ade722c8979635e5d3fadb3
Hima Rani Mathews <68076769+HimaRaniMathews@users.noreply.github.com>
2022-06-07 23:35:43 +0530

    Time: 0 ms (100.00%), Space: 43.2 MB (14.39%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -1,18 +1,19 @@
-class Solution {
-   public void merge(int A[], int m, int B[], int n) {
-        int insertIndex = m+n-1;
-        int indexA = m-1,indexB = n-1;
-        
-        while(indexB>=0){
-            if(indexA<0){
-                A[insertIndex--] = B[indexB--];
+public class Solution {
+    public void merge(int A[], int m, int B[], int n) {
+ 
+        while(m > 0 && n > 0){
+            if(A[m-1] > B[n-1]){
+                A[m+n-1] = A[m-1];
+                m--;
             }else{
-                if(B[indexB]>=A[indexA]){
-                    A[insertIndex--] = B[indexB--];
-                }else{
-                    A[insertIndex--] = A[indexA--];
-                }
+                A[m+n-1] = B[n-1];
+                n--;
             }
+        }
+ 
+        while(n > 0){
+            A[m+n-1] = B[n-1];
+            n--;
         }
     }
 }
