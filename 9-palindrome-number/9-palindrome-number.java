92f262f935a5014c1f5915f1dda6fc0b569ac8ee
Hima Rani Mathews <68076769+HimaRaniMathews@users.noreply.github.com>
2022-07-08 21:12:33 +0530

    Time: 17 ms (33.37%), Space: 44.8 MB (49.27%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        int m=x;
+        int s=0;
+        while(x!=0){
+            s=s*10+(x%10);
+            x=x/10;
+        }
+        if(s==m)
+            return true;
+        else
+            return false;
+    }
+}
